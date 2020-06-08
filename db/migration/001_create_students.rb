17b7f2d189a18b8f6e57fd1493fa1e05aabcbf81
William Mittard <billymittard@ymail.com>
2020-06-08 15:26:21 +0000

    Automatically backed up by Learn

--- db/migration/001_create_students.rb
+++ db/migration/001_create_students.rb
@@ -1,10 +1,10 @@
-class CreateStudents < AcitveRecord::Migration
+class CreateStudents < ActiveRecord::Migration
   def change
-  create_table :students do |t|
-    t.string :first_name 
-    t.string :last_name 
-    
-    t.timestamps null: false 
-  end 
-end 
-end 
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+ 
+      t.timestamps null: false
+    end
+  end
+end
